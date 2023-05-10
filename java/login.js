04a682cfbdf1e1e196b94ed1f0c554e0b1dc31d9
gordonnrat <gordonnrat@gmail.com>
2023-05-09 22:36:12 -0700

    changes to everything

--- java/login.js
+++ java/login.js
@@ -1,7 +1,21 @@
-var modal = document.getElementById('login');
+const modal = document.getElementById('login');
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+const loggedIn = document.querySelectorAll("#login");
+const loginButton = document.querySelector("#login-button");
+const nextButton = document.querySelector("#nextbutton");
+
+var username;
+
+nextButton.addEventListener("click", () => {
+  username = window.document.getElementById("username").value;
+  loginButton.innerHTML = `@${username}`;
+  modal.style.display = "none";
+}
+)
+
