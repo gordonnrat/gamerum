68af53574209e315f85fc44b856612fff589cf45
gordonnrat <gordonnrat@gmail.com>
2023-05-10 22:46:19 -0700

    working log out and changed login box

--- java/login.js
+++ java/login.js
@@ -6,22 +6,53 @@ window.onclick = function(event) {
   }
 }
 
+logIn = () => {
+  document.getElementById('login').style.display='block'
+  usernameInput.style.display= "inline";
+  logOut()
+}
+
 const loggedIn = document.querySelectorAll("#login");
 const loginButton = document.querySelector("#login-button");
 const nextButton = document.querySelector("#nextbutton");
+const loginBox = document.querySelector("#login-box");
+const loginTitle = document.querySelector("#login-title");
+const usernameInput = document.querySelector("#username")
 
 let username;
 
+window.addEventListener("load", () => {
+  if(username != "Login"){
+    username = localStorage.getItem('username');
+    loginButton.innerHTML = `${username}`;
+  } else if(username == "Login"){
+    username = "Login";
+    loginButton.innerHTML = "Login";
+  }
+})
+
 nextButton.addEventListener("click", () => {
-  username = window.document.getElementById("username").value;
-  loginButton.innerHTML = `@${username}`;
-  localStorage.setItem('username', username);
-  modal.style.display = "none";
+  console.log(username);
+  if(username == "Login"){
+    username = window.document.getElementById("username").value;
+    loginButton.innerHTML = `${username}`;
+    localStorage.setItem('username', username);
+    modal.style.display = "none";
+  } else if (username != "Login"){
+    username = "Login";
+    loginButton.innerHTML = `${username}`;
+    modal.style.display = "none";
+    localStorage.setItem('username', "Login");
+  }
 }
 )
 
-window.addEventListener("load", () => {
-  username = localStorage.getItem('username');
-  loginButton.innerHTML = `@${username}`;
-})
-
+logOut = () => {
+  if(username != "Login") {
+    loginTitle.innerHTML = "Sign Out?";
+    usernameInput.style.display= "none";
+  } else if(username == "Login") {
+    loginTitle.innerHTML = "Sign in to Gamerum";
+    usernameInput.style.display= "inline";
+  }
+}
