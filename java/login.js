89d9f36d5a23f949b3aa71b931e23b65efd2e445
gordonnrat <gordonnrat@gmail.com>
2023-05-10 20:42:23 -0700

    added localstorage stuff for username

--- java/login.js
+++ java/login.js
@@ -10,12 +10,18 @@ const loggedIn = document.querySelectorAll("#login");
 const loginButton = document.querySelector("#login-button");
 const nextButton = document.querySelector("#nextbutton");
 
-var username;
+let username;
 
 nextButton.addEventListener("click", () => {
   username = window.document.getElementById("username").value;
   loginButton.innerHTML = `@${username}`;
+  localStorage.setItem('username', username);
   modal.style.display = "none";
 }
 )
+
+window.addEventListener("load", () => {
+  username = localStorage.getItem('username');
+  loginButton.innerHTML = `@${username}`;
+})
 
